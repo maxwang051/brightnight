7f44aa2a8e54e75f7173623d5388d8406abe3255
Max Wang <maxwang051@gmail.com>
2016-01-20 00:00:49 -0600

    added urls and home page with map

--- brightnight/urls.py
+++ brightnight/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from find import views as find_views
+from find import urls as find_urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', find_views.home_page, name='home'),
+    url(r'^find/', include(find_urls)),
 ]
---
+++ find/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url, include
+from find import views
+
+urlpatterns = [
+    #url(r'^$', views.main_map, name='main_map'),
+]
